13d278b0a203d21b19dbee773cec605cf6c73b6d
lisjump <lisjump@gmail.com>
2019-02-06 20:47:51 -0700

    Working tank car, motor, and queueitem classes

--- ev3/ev3motor.py
+++ ev3/ev3motor.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 from ev3dev.ev3 import *
-import math
+import math 
+import datetime 
 
+class QueueItem():
+  def __init__(self, startaction, startkwargs = None, stopaction = None, stopkwargs = None, runtime = None):
+    self.startaction = startaction
+    self.startkwargs = startkwargs
+    self.stopaction = stopaction
+    self.stopkwargs = startkwargs
+    self.runtime = runtime
+    self.stoptime = None
+
+class Queue():
+  def __init__(self):
+    self.queue = []
 
 class MyMotor(Motor):
   def __init__(self, port, reversed = 0, tireradius = None, defaultSpeed = 0):
@@ -13,6 +26,8 @@ class MyMotor(Motor):
       self.polarity = "inversed"
     self.tireradius = tireradius
     self.defaultSpeed = defaultSpeed
+    self.stoptime = None
+    self.stop_action = self.STOP_ACTION_BRAKE
 
   def tachosperinch(self):
     if self.tireradius:
@@ -21,15 +36,15 @@ class MyMotor(Motor):
       print("Error: need tire radius")
       return
 
-  def runMotorInchSecond(self, inches, seconds):
+  def runMotorInchSecond(self, inches, seconds, wait = True):
     speed = self.tachosperinch() * inches / seconds
     if (speed > self.max_speed):
       print("Error: exceeds max speed")
       return
     else:
-      self.runMotorSecondSpeed(speed = speed, seconds = seconds)
+      self.runMotorSecondSpeed(speed = speed, seconds = seconds, wait = wait)
 
-  def runMotorSpeedInch(self, inches, speed = None):
+  def runMotorSpeedInch(self, inches, speed = None, wait = True):
     if not speed and not self.defaultSpeed:
       print("Error: no speed given")
       return
@@ -40,10 +55,10 @@ class MyMotor(Motor):
       speed = -1 * abs(speed)
     else:
       speed = abs(speed)
-    self.runMotorSecondSpeed(seconds = seconds, speed = speed)
+    self.runMotorSecondSpeed(seconds = seconds, speed = speed, wait = wait)
     return seconds
   
-  def runMotorSecondSpeed(self, seconds, speed = None, wait=True):
+  def runMotorSecondSpeed(self, seconds, speed = None, wait = True):
     if not speed and not self.defaultSpeed:
       print("Error: no speed given")
       return
@@ -51,6 +66,7 @@ class MyMotor(Motor):
       speed = self.defaultSpeed
     if wait:
       self.wait_until_not_moving()
+    self.stoptime = datetime.datetime.now() + datetime.timedelta(seconds = seconds)
     self.run_timed(time_sp=seconds*1000, speed_sp=speed)
 
 
@@ -60,8 +76,9 @@ class TankCar():
     self.left = left
     self.right = right
     self.axlelength = axlelength
+    self.stoptime = None
     
-  def goStraight(self, speed = None, seconds = None, inches = None):
+  def goStraight(self, speed = None, seconds = None, inches = None, wait = True):
     if speed and inches and seconds:
       print("Error: too many constraints")
       return
@@ -69,29 +86,68 @@ class TankCar():
       print("Error: exceeds max speed")
       return    
     elif speed and seconds:
-      self.left.runMotorSecondSpeed(seconds = seconds, speed = speed)
-      self.right.runMotorSecondSpeed(seconds = seconds, speed = speed)
+      self.left.runMotorSecondSpeed(seconds = seconds, speed = speed, wait = wait)
+      self.right.runMotorSecondSpeed(seconds = seconds, speed = speed, wait = wait)
     elif inches and not (self.left.tireradius and self.right.tireradius):
       print("Error: set tire radius if using inches")
       return
     elif inches and seconds:
-      self.left.runMotorInchSecond(seconds = seconds, inches = inches)
-      self.right.runMotorInchSecond(seconds = seconds, inches = inches)
+      self.left.runMotorInchSecond(seconds = seconds, inches = inches, wait = wait)
+      self.right.runMotorInchSecond(seconds = seconds, inches = inches, wait = wait)
     elif inches and speed:
-      self.left.runMotorSpeedInch(inches = inches, speed = speed)
-      self.right.runMotorSpeedInch(inches = inches, speed = speed)
+      self.left.runMotorSpeedInch(inches = inches, speed = speed, wait = wait)
+      self.right.runMotorSpeedInch(inches = inches, speed = speed, wait = wait)
     elif inches:
-      self.left.runMotorSpeedInch(inches = inches)
-      self.right.runMotorSpeedInch(inches = inches)
+      self.left.runMotorSpeedInch(inches = inches, wait = wait)
+      self.right.runMotorSpeedInch(inches = inches, wait = wait)
     elif seconds:
-      self.left.runMotorSecondSpeed(seconds = seconds)
-      self.right.runMotorSecondSpeed(seconds = seconds)
+      self.left.runMotorSecondSpeed(seconds = seconds, wait = wait)
+      self.right.runMotorSecondSpeed(seconds = seconds, wait = wait)
+    self.stoptime = self.left.stoptime
+    return(self.stoptime)
   
-  def TightTurn(self, direction, degrees = None, seconds = None, speed = None):
+  def TightTurn(self, direction, degrees = None, seconds = None, speed = None, wait = True):
     if direction.lower() not in ["left", "right"]:
       print("Error: directions must be 'left' or 'right'")
-    elif degrees and seconds and speed:
+      return
+    elif direction.lower() == "left":
+      leftparity = -1
+      rightparity = 1
+    else:
+      leftparity = 1
+      rightparity = -1
+
+    if degrees and not (self.left.tireradius and self.right.tireradius and self.axlelength):
+      print("Error: must have tire radius and axle length set to calculate degrees")
+      return
+    else:
+      inches = self.axlelength * math.pi * degrees/360
+
+    if degrees and seconds and speed:
       print("Error: too many constraints")
     elif speed and (speed > self.left.max_speed) or (speed > self.right.max_speed):
       print("Error: exceeds max speed")
-      return    
+    elif speed and seconds:
+      self.left.runMotorSecondSpeed(seconds = seconds, speed = leftparity * speed, wait = wait)
+      self.right.runMotorSecondSpeed(seconds = seconds, speed = rightparity * speed, wait = wait)
+    elif degrees and seconds:
+      self.left.runMotorInchSecond(seconds = seconds, inches = inches, wait = wait)
+      self.right.runMotorInchSecond(seconds = seconds, inches = inches, wait = wait)
+    elif degrees and speed:
+      self.left.runMotorSpeedInch(inches = inches, speed = leftparity * speed, wait = wait)
+      self.right.runMotorSpeedInch(inches = inches, speed = rightparity * speed, wait = wait)
+    elif degrees:
+      self.left.runMotorSpeedInch(inches = leftparity * inches, wait = wait)
+      self.right.runMotorSpeedInch(inches = rightparity * inches, wait = wait)
+    elif seconds and (self.left.defaultSpeed and self.right.defaultSpeed):
+      self.left.runMotorSecondSpeed(seconds = seconds, speed = leftparity * self.left.defaultSpeed, wait = wait)
+      self.right.runMotorSecondSpeed(seconds = seconds, speed = rightparity * self.right.defaultSpeed, wait = wait)
+    
+    self.stoptime = self.left.stoptime
+    return(self.stoptime)
+
+  def Stop(self):
+    self.left.stop()
+    self.right.stop()
+    self.stoptime = datetime.datetime.now()
+    return(self.stoptime)
--- test.py
+++ test.py
@@ -2,15 +2,72 @@
 
 import ev3.ev3motor as ev3
 import importlib
+import datetime
 
 left = ev3.MyMotor(port = "B", tireradius = .73)
 right = ev3.MyMotor(port = "C", tireradius = .73)
-car = ev3.TankCar(left=left, right=right)
-car.goStraight(speed=500, seconds=3)
-car.goStraight(speed=-500, seconds=3)
-car.goStraight(speed=500, inches=10)
-car.goStraight(speed=500, inches=-10)
-car.goStraight(inches=10, seconds=1.569)
-car.goStraight(inches=-10, seconds=1.569)
+car = ev3.TankCar(left=left, right=right, axlelength=3.9)
+movequeue = []
+queueitem = ev3.QueueItem(startaction = car.goStraight, startkwargs = {"speed": 500, "seconds": 3, "wait": False})
+movequeue.append(queueitem)
+queueitem = ev3.QueueItem(startaction = car.goStraight, startkwargs = {"speed": -500, "seconds": 3, "wait": False})
+# movequeue.append(queueitem)
+# queueitem = ev3.QueueItem(startaction = car.goStraight, startkwargs = {"speed": 500, "inches": 10, "wait": False})
+# movequeue.append(queueitem)
+# queueitem = ev3.QueueItem(startaction = car.goStraight, startkwargs = {"speed": 500, "inches": -10, "wait": False})
+# movequeue.append(queueitem)
+# queueitem = ev3.QueueItem(startaction = car.goStraight, startkwargs = {"inches": 5, "seconds": 1, "wait": False})
+# movequeue.append(queueitem)
+# queueitem = ev3.QueueItem(startaction = car.goStraight, startkwargs = {"inches": -5, "seconds": 1, "wait": False})
+# movequeue.append(queueitem)
 
-# comment
+touch = ev3.TouchSensor()
+sound = ev3.Sound()
+color = ev3.ColorSensor()
+gyro = ev3.GyroSensor()
+
+
+
+activeitem = False
+while True:
+  if not activeitem or datetime.datetime.now() >= activeitem.stoptime:
+    if activeitem and activeitem.stopaction:
+      activeitem.stopaction(**activeitem.stopkwargs)
+    if len(movequeue) > 0:
+      activeitem = movequeue.pop(0)
+      activeitem.stoptime = activeitem.startaction(**activeitem.startkwargs)
+      if activeitem.runtime:
+          activeitem.stoptime = datetime.datetime.now() + timedelta("seconds = " + command[4])
+
+  if touch.is_pressed:
+    sound.play_song((
+      ('D4', 'e3'),
+      ('D4', 'e3'),
+      ('D4', 'e3'),
+      ('G4', 'h'),
+      ('D5', 'h')
+    ))
+
+
+  if color.color == 1:
+    sound.play_song((
+      ('D4', 'e3'),      # intro anacrouse
+      ('D4', 'e3'),
+      ('D4', 'e3'),
+      ('G4', 'h'),       # meas 1
+      ('D5', 'h'),
+      ('C5', 'e3'),      # meas 2
+      ('B4', 'e3'),
+      ('A4', 'e3'),
+      ('G5', 'h'),
+      ('D5', 'q'),
+      ('C5', 'e3'),      # meas 3
+      ('B4', 'e3'),
+      ('A4', 'e3'),
+      ('G5', 'h'),
+      ('D5', 'q'),
+      ('C5', 'e3'),      # meas 4
+      ('B4', 'e3'),
+      ('C5', 'e3'),
+      ('A4', 'h.'),
+    ))
